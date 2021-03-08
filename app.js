fdbde64c47cfb94e4433c8503226f1370190de45
Mélusine Rey <zelumine@live.fr>
2021-03-08 15:53:33 +0100

    Add registerPartials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const hbs = require('hbs');
 
 const indexRouter = require('./routes/index');
 const patientsRouter = require('./routes/patient-route');
@@ -13,6 +14,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(logger('dev'));
 app.use(express.json());
