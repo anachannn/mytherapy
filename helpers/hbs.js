3f2a2bfc847339eea1c9467ed872db5c06e62804
Mathoz <mathieu.lambertin03@gmail.com>
2021-03-09 16:50:23 +0100

    new nav bar

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -4,26 +4,26 @@ Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
 
   switch (operator) {
       case '==':
-          return (v1 == v2) ? options.inverse(this) : options.fn(this);
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
       case '===':
-          return (v1 === v2) ? options.inverse(this) : options.fn(this);
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
       case '!=':
-          return (v1 != v2) ? options.inverse(this) : options.fn(this);
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
       case '!==':
-          return (v1 !== v2) ? options.inverse(this) : options.fn(this);
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
       case '<':
-          return (v1 < v2) ? options.inverse(this) : options.fn(this);
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
       case '<=':
-          return (v1 <= v2) ? options.inverse(this) : options.fn(this);
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
       case '>':
-          return (v1 > v2) ? options.inverse(this) : options.fn(this);
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
       case '>=':
-          return (v1 >= v2) ? options.inverse(this) : options.fn(this);
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
       case '&&':
-          return (v1 && v2) ? options.inverse(this) : options.fn(this);
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
       case '||':
-          return (v1 || v2) ? options.inverse(this) : options.fn(this);
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
       default:
-          return options.fn(this);
+          return options.inverse(this);
   }
 });
