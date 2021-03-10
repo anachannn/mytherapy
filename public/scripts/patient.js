14363400752279f338670104f895e8b6f3661546
Mathoz <mathieu.lambertin03@gmail.com>
2021-03-10 12:10:52 +0100

    try the calendar and fail :'(

--- public/scripts/patient.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello from the patient.js siiiiiiide!");
-
-// const btnAddDocument = document.getElementById("add-doc");
-
-// function testClick() {
-//   console.log("button clicked");
-// }
-
-// btnAddDocument.onclick = testClick;
