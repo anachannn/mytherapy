5e87b39a92417ae1293a164b5795a3ff1f02dcf6
Mélusine Rey <zelumine@live.fr>
2021-03-10 12:19:45 +0100

    Push DOM script

--- public/scripts/patient.js
+++ public/scripts/patient.js
@@ -1,9 +1,26 @@
-console.log("Hello from the patient.js siiiiiiide!");
+/* VARIABLES */
+const btnAddDocument = document.getElementById("add-doc");
+const inputEditTherapist = document.getElementById("input-edit-myTherapist");
+const selectEditTherapist = document.getElementById("select-edit-myTherapist");
 
-// const btnAddDocument = document.getElementById("add-doc");
 
-// function testClick() {
-//   console.log("button clicked");
+
+/* FUNCTIONS */
+function testClick() {
+  console.log("button clicked");
+}
+
+// function toggleHidden(event, element) {
+//   console.log(event.target);
+//   event.target.classList.add("hidden");
+//   selectEditTherapist.classList.remove("hidden");
 // }
 
+
+
+
+
+
+/* EVENT LISTENERS */
 // btnAddDocument.onclick = testClick;
+// inputEditTherapist.onclick = toggleHidden;
