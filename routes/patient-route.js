34d2932abf4970861b9931bd14703bab0bdfa24d
Mélusine Rey <zelumine@live.fr>
2021-03-11 12:56:21 +0100

    Fix delete document route

--- routes/patient-route.js
+++ routes/patient-route.js
@@ -7,6 +7,7 @@ const DoctorModel = require('./../models/doctor.model');
 const TextModel = require('./../models/text.model');
 const LoopModel = require('./../models/loop.model');
 let currentDoctor, newDoctor;
+const moment = require("moment"); // usefull to format date
 
 /* LOG IN FIRST */
 router.get('/', (req, res, next) => {
@@ -32,7 +33,6 @@ router.get('/edit-profile/:id', (req, res, next) => {
     PatientModel.findById(req.params.id)
     .populate("myTherapist myTexts myLoops")
     .then(dbRes => {
-      console.log("dbRes after findbyId and populate: ", dbRes);
       currentDoctor = dbRes.myTherapist._id;
       res.render("editPatientProfile", { patientInfo: dbRes, doctorsList })
     })
@@ -42,13 +42,9 @@ router.get('/edit-profile/:id', (req, res, next) => {
 });
 
 router.post('/edit-profile/:id', (req, res, next) => {
-  console.log(req.params.id);
   PatientModel.findById(req.params.id)
   .then(editRes => {
     newDoctor = req.body.myTherapist;
-    console.log("---------- NEW TRY ----------");
-    console.log("current doctor:", currentDoctor);
-    console.log("new doctor: ", newDoctor);
     if (newDoctor !== currentDoctor) {
       DoctorModel.findByIdAndUpdate(currentDoctor, { $pull: {myPatients: editRes._id} })
       .then(() => {
@@ -132,9 +128,9 @@ router.post("/add-document/:type", (req, res, next) => {
   const patientId = req.session.currentUser._id;
   console.log(patientId);
   if(req.params.type === "text") {
-    console.log("inside if text");
-    console.log(req.body);
     const { date, title, text } = req.body;
+    moment(date).format("YYYY-MM-DD");
+
     TextModel.create({ patientId, date, title, text })
     .then(dbRes => {
       PatientModel.findByIdAndUpdate(patientId, { $push: {myTexts: dbRes._id} })
@@ -161,6 +157,7 @@ router.post("/add-document/:type", (req, res, next) => {
         alternativeThoughts
       } = req.body;
     
+      moment(date).format("YYYY-MM-DD");
       LoopModel.create({
         patientId,
         date,
@@ -191,26 +188,12 @@ router.post("/add-document/:type", (req, res, next) => {
 /* UPDATE ENTRY/DOCUMENT */
 router.get("/edit-document/:type/:id", (req, res, next) => {
   if (req.params.type === "text") {
-//     TextModel.findById(req.params.id)
-//     .then(dbRes => {
-//     res.render("documents/editTextDocument", {text:dbRes})
-//   })
-//   .catch(error => next(error))
-//   } else if (req.params.type === "loop"){
-//     LoopModel.findById(req.params.id)
-//     .then(dbRes => {
-//       console.log(dbRes)
-//     res.render("documents/editLoopDocument", {loop:dbRes})
-//   })
-//   .catch(error => next(error))
-// }
-// })
-    TextModel.findByIdAndUpdate(req.params.id)
+    TextModel.findById(req.params.id)
     .then(textToUpdate => res.render("documents/editTextDocument", {text: textToUpdate, title: "MyTherapy | Update text document"}))
     .catch(next);
   }
   else if (req.params.type === "loop") {
-    LoopModel.findByIdAndUpdate(req.params.id)
+    LoopModel.findById(req.params.id)
     .then(loopToUpdate => res.render("documents/editLoopDocument", {loop: loopToUpdate, title: "MyTherapy | Update loop document"}))
     .catch(next);
   }
@@ -222,8 +205,7 @@ router.post("/edit-document/:type/:id", (req, res, next) => {
     const { date, title, text } = req.body;
     TextModel.findByIdAndUpdate(req.params.id, { date, title, text })
     .then(dbRes => {
-      console.log(dbRes);
-      console.log("Document updated!");
+      console.log("Document updated! ", dbRes);
       res.redirect("/patient/dashboard");
     })
     .catch(next);
@@ -260,7 +242,7 @@ router.post("/edit-document/:type/:id", (req, res, next) => {
         alternativeThoughts
       })
       .then(dbRes => {
-        console.log("Document successfully updated");
+        console.log("Document successfully updated! ", dbRes);
         res.redirect("/patient/dashboard");
       })
       .catch(next);
@@ -272,19 +254,38 @@ router.post("/edit-document/:type/:id", (req, res, next) => {
 /* DELETE ENTRY/DOCUMENT */
 router.get('/document-delete/:type/:id', (req, res, next) => {
   if(req.params.type === "text") {
-    TextModel.findByIdAndDelete(req.params.id)
+    TextModel.findById(req.params.id)
+    .then(textToDelete => {
+      PatientModel.findByIdAndUpdate(textToDelete.patientId, { $pull: {myTexts: textToDelete._id}})
+      .then(console.log("Text document deleted"))
+      .catch(next);
+    })
     .then(dbRes => {
+      TextModel.findByIdAndDelete(req.params.id)
+      .then(dbRes => {
       console.log("Document successfully deleted");
       res.redirect("/patient/dashboard");
+      })
+      .catch(next);
     })
     .catch(next);
+    
   } else if (req.params.type === "loop") {
+    LoopModel.findById(req.params.id)
+    .then(loopToDelete => {
+      PatientModel.findByIdAndUpdate(loopToDelete.patientId, { $pull: {myLoops: loopToDelete._id}})
+      .then(console.log("Loop document deleted"))
+      .catch(next);
+    })
+    .then(dbRes => {
       LoopModel.findByIdAndDelete(req.params.id)
       .then(dbRes => {
         console.log("Document successfully deleted");
         res.redirect("/patient/dashboard");
       })
       .catch(next);
+    })
+    .catch(next);
   }
 });
 
