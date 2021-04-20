3f688ad235c00f4ced26da941e7e80903f255736
Mathoz <mathieu.lambertin03@gmail.com>
2021-04-20 14:18:21 +0200

    fixing model password porblem

--- models/patient.model.js
+++ models/patient.model.js
@@ -20,8 +20,7 @@ const patientSchema = new Schema({
     },
     password: {
         type: String,
-        required: true,
-        unique: true,
+        required: true
     },
     phoneNumber: String,
     location: {
